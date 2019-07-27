cf423e6bb6efa9c2e7af9ac12b2960760dc92454
zeshan-iqbal <link2zeshan@gmail.com>
2019-07-27 12:01:46 -0700

    setup redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,9 +1,33 @@
 import React, { Component } from "react";
 
 class CoursesPage extends Component {
-  state = {};
+  state = {
+    course: { title: "" }
+  };
+
+  handleChange = ({ target }) => {
+    const course = { ...this.state.course, title: target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
+import configureStore from "./redux/configureStore";
+
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
